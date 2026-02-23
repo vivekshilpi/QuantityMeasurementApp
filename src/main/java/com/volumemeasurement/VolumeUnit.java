e3bdaba0760dae661e1d96087de7a0d10986dad2
vivekshilpi <vivekshilpi1234@gmail.com>
2026-02-24 00:38:35 +0530

    [Vivek] : Added UC -11 Volume measurement

---
+++ src/main/java/com/volumemeasurement/VolumeUnit.java
@@ -0,0 +1,38 @@
+package com.volumemeasurement;
+
+import com.measurement.IMeasurable;
+
+public enum VolumeUnit implements IMeasurable {
+
+    LITRE(1.0, "Litre"),
+    MILLILITRE(0.001, "Millilitre"),
+    GALLON(3.78541, "Gallon");
+
+    private final double conversionFactor;
+    private final String unitName;
+
+    VolumeUnit(double conversionFactor, String unitName) {
+        this.conversionFactor = conversionFactor;
+        this.unitName = unitName;
+    }
+
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    @Override
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return unitName;
+    }
+}
