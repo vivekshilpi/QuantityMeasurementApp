e7555b788dc9580f18d7c2221d9295f6d8f3d030
vivekshilpi <vivekshilpi1234@gmail.com>
2026-04-13 23:40:14 +0530

    [Vivek] : Modified Security Configuration file

--- src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
@@ -79,7 +79,8 @@ public class SecurityConfig {
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
                 "http://127.0.0.1:3000",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "http://localhost:4200"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
